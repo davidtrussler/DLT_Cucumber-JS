392a8eb674cce662548f56865ed760675e3e2dff
David Trussler <mail@davidtrussler.net>
2019-07-23 09:08:45 +0100

    Adds webpage step definitions

---
+++ features/step_definitions/webpage.js
@@ -0,0 +1,19 @@
+const defineSupportCode = require('cucumber').defineSupportCode;
+const assert = require('assert');
+
+defineSupportCode(function({Given, Then, When}) {
+	Given('I am on the web page', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+
+	When('I change the dropdown to green', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+
+	Then('the colour of the test box is green', function () {
+     // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+   });
+}); 
