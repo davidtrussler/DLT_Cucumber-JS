17da91a0fb09a4b511b9d00ebf8ce78dd52be0db
David Trussler <mail@davidtrussler.net>
2019-07-26 16:55:06 +0100

    Updates feature to load webpage

--- features/step_definitions/webpage.js
+++ features/step_definitions/webpage.js
@@ -1,19 +1,19 @@
 const defineSupportCode = require('cucumber').defineSupportCode;
 const assert = require('assert');
+const seleniumWebdriver = require('selenium-webdriver');
 
 defineSupportCode(function({Given, Then, When}) {
-	Given('I am on the web page', function () {
-     // Write code here that turns the phrase above into concrete actions
-     return 'pending';
-   });
+  Given('I am on the web page', function () {
+    return this.driver.get('http://localhost:5000');
+  });
 
-	When('I change the dropdown to green', function () {
-     // Write code here that turns the phrase above into concrete actions
-     return 'pending';
-   });
+  When('I change the dropdown to green', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
 
-	Then('the colour of the test box is green', function () {
-     // Write code here that turns the phrase above into concrete actions
-     return 'pending';
-   });
-}); 
+  Then('the colour of the test box is green', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+});
